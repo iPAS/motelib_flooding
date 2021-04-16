36c9033bae3ae21eaf6692e26000b58decb9b984
iPAS <ptiwatthanont@gmail.com>
2021-04-16 16:54:52 +0700

    Separate some code to flood_routing.h.

--- flood.c
+++ flood.c
@@ -7,19 +7,8 @@
 #include <motelib/uart.h>
 #include <pt/pt.h>
 
+#include "flood_routing.h"
 
-#define FLOOD_MSG_TYPE  0x01
-#define REPORT_MSG_TYPE 0x22
-
-#define MAX_HOP 255
-#define WAIT_PARENT 10000
-
-
-typedef struct flood_msg
-{
-    uint16_t seqNo;
-    uint8_t  hopCount;
-} FloodMsg;
 
 Timer    delayTimer;
 uint16_t currentSeq;
