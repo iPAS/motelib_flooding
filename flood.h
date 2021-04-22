57d1e6faaa53b7444a8c2dcdd2b83958de8dbfea
iPAS <ptiwatthanont@gmail.com>
2021-04-22 20:56:58 +0700

    Get rid of unimportance.

--- flood.h
+++ flood.h
@@ -4,17 +4,15 @@
 
 #include "all_headers.h"
 
+
 #define FLOOD_MSG_TYPE  0x01
-#define REPORT_MSG_TYPE 0x22
-
 #define MAX_HOP         255
-#define WAIT_PARENT     10000
 
-typedef struct
-{
-    uint8_t SeqNo;
-    uint8_t hopCount;
-} RoutingMsg;
+typedef struct {
+    Address     origin;
+    uint8_t     hop_count;
+    uint32_t    timestamp;
+} flood_header_st;
 
 
 extern void flood_init(void);
--- test_comm.c
+++ test_comm.c
@@ -1,16 +1,13 @@
 #include "flood.h"
 
 
-void button(ButtonStatus s)
-{
+void button(ButtonStatus s) {
     // Toggle LED#1 whenever the button is pressed
-    if (s == BUTTON_PRESSED)
-        ledToggle(1);
+    if (s == BUTTON_PRESSED) ledToggle(1);
 }
 
 
-void boot()
-{
+void boot() {
     buttonSetHandler(button);
 
     debug("Booting.. Addr:%d, PAN_ID:%d, CH:%d", getAddress(), getPanId(), getChannel());
