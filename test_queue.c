7ec222f5da26a15b45e83e6f4b835616191c20b7
iPAS <ptiwatthanont@gmail.com>
2021-04-30 19:11:17 +0700

    Harness test.

--- test_queue.c
+++ test_queue.c
@@ -2,12 +2,41 @@
 
 #include "queue.h"
 
+#define TEST_COUNT 1000000
 queue_t q;
 
 int main()
 {
+    char *messages[] =
+    {
+        "hello one",
+        "two worlds",
+        "Tree for three",
+    };
+
     q_init(&q);
 
-    // q_en(q)
+    int loop = TEST_COUNT;
+    while (loop-- > 0)
+    {
+        printf("loop: %d\n", TEST_COUNT-loop);
+        int i;
+
+        for (i = 0; i < sizeof(messages)/sizeof(char *); i++)
+        {
+            q_enqueue(&q, messages[i], strlen(messages[i]));
+            printf("In queue: '%s' -- queue len = %d\n", messages[i], q_length(&q));
+        }
+
+        char buffer[11];
+        buffer[sizeof(buffer)-1] = '\0';
+        uint8_t len;
+
+        for (i = 0; i < sizeof(messages)/sizeof(char *); i++)
+        {
+            len = q_dequeue(&q, buffer, sizeof(buffer)-1);
+            printf("Out queue: '%s' -- queue left = %d\n", buffer, q_length(&q));
+        }
+    }
     return 0;
 }
