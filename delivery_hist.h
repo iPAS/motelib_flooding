3e52957149895ba8f89a172cdcb74df4bb30c17a
iPAS <ptiwatthanont@gmail.com>
2021-05-03 17:52:30 +0700

    Separate the code.

--- delivery_hist.h
+++ delivery_hist.h
@@ -3,7 +3,20 @@
 
 
 #include "all_headers.h"
+#include "flood.h"
 
+typedef struct
+{
+    bool available;
+    uint32_t timestamp;
+
+    RoutingHeader latestHdr;  // Received header.
+    Address parent;         // Intermediate node who sent us. Our way back to the source.
+    uint8_t currSeqNo;      // Current 'seqNo' corresponding with the 'originSource'.
+} delivery_history_t;
+
+extern void hist_init();
+extern delivery_history_t *hist_find(RoutingHeader *hdr);
 
 
 #endif  // __DELIVERY_HIST_H__
